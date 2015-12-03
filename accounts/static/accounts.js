65dc426ff84a536b462659d237472235accaf9e0
fcartu <fcartu@hotmail.com>
2015-12-03 13:43:18 -0500

    Implementing logout feature

--- accounts/static/accounts.js
+++ accounts/static/accounts.js
@@ -4,6 +4,10 @@ var initialize = function(navigator, user, token, urls) {
         navigator.id.request();    
     });
 
+    $('#id_logout').on('click', function() {
+        navigator.id.logout();
+    });
+
     navigator.id.watch({
         loggedInUser: user,
         onlogin: function(assertion) {
@@ -14,7 +18,14 @@ var initialize = function(navigator, user, token, urls) {
             deferred.done(function() { window.location.reload(); });
             deferred.fail(function() { navigator.id.logout(); });
         },
-        onlogout: function() {}
+        onlogout: function() {
+            $.post(
+                urls.logout,
+                { csrfmiddlewaretoken: token }
+            )
+                .done(function() { window.location.reload() })
+                .fail(function() { navigator.id.logout() })
+        }
     });
 };
 
